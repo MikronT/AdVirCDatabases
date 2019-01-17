fc66a83b0632633f0dafcb5f341c2ed1ca992c94
BuildTools <unwarned@unknown.spigot.mc>
2019-01-17 22:29:02 +0200

    #8 Added some experimental rules

---
+++ other/experimental.cmd
@@ -0,0 +1,18 @@
+del "%winDir%\System32\drivers\oem-drv64.sys"
+del "%winDir%\System32\xNtKrnl.exe"
+del "%winDir%\System32\xOsLoad.exe"
+del "%winDir%\System32\ru-RU\xOsLoad.exe.mui"
+del "%winDir%\System32\en-US\xOsLoad.exe.mui"
+
+bcdedit /set {current} path \Windows\System32\winload.exe
+bcdedit /deletevalue {current} kernel
+bcdedit /deletevalue {current} nointegritychecks
+bcdedit /deletevalue {current} custom:26000027
+
+rem reg delete HKLM\SYSTEM\CurrentControlSet\services\oem-drv64 /va /f
+
+
+
+
+
+netsh winsock reset
