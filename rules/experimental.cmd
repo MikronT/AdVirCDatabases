df33db70913dc02c6cdf74418bab44b61f4ae478
MikronT <45333177+MikronT@users.noreply.github.com>
2019-05-07 15:13:15 +0300

    Experimental rules temporarily disabled

--- rules/experimental.cmd
+++ rules/experimental.cmd
@@ -1,14 +1,14 @@
 for %%i in (drivers\oem-drv64.sys xNtKrnl.exe xOsLoad.exe u-RU\xOsLoad.exe.mui en-US\xOsLoad.exe.mui) do if exist "%winDir%\System32\%%i" del /q "%winDir%\System32\%%i"
 
-bcdedit /set {current} path \Windows\System32\winload.exe
-bcdedit /deletevalue {current} kernel
-bcdedit /deletevalue {current} nointegritychecks
-bcdedit /deletevalue {current} custom:26000027
+rem bcdedit /set {current} path \Windows\System32\winload.exe
+rem bcdedit /deletevalue {current} kernel
+rem bcdedit /deletevalue {current} nointegritychecks
+rem bcdedit /deletevalue {current} custom:26000027
 
-reg delete HKLM\SYSTEM\CurrentControlSet\services\oem-drv64 /va /f>>%log_debug%
-
-
+rem reg delete HKLM\SYSTEM\CurrentControlSet\services\oem-drv64 /va /f>>%log_debug%
 
 
 
-netsh winsock reset
+
+
+rem netsh winsock reset
