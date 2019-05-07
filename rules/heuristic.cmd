d6f8842610c47b38c58a92d2d247fc4e941f5fe0
MikronT <45333177+MikronT@users.noreply.github.com>
2019-05-07 15:13:31 +0300

    Heuristic rules temporarily disabled

--- rules/heuristic.cmd
+++ rules/heuristic.cmd
@@ -1,3 +1,3 @@
-for /f "delims=" %%i in (%dataDir%\databases\rewrited\dirs\shortcuts.db) do if exist "%%i" attrib /s -r -s "%%i\*.lnk"
+rem for /f "delims=" %%i in (%dataDir%\databases\rewrited\dirs\shortcuts.db) do if exist "%%i" attrib /s -r -s "%%i\*.lnk"
 
 sfc /scannow
