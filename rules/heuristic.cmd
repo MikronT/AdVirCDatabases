e93a3f0a9cd2fd07dd1f07f40825152b802c4cdd
MikronT <45333177+MikronT@users.noreply.github.com>
2019-07-31 13:42:08 +0300

    Fixed shortcuts related errors

--- rules/heuristic.cmd
+++ rules/heuristic.cmd
@@ -1,4 +1,4 @@
-rem for /f "delims=" %%i in (%dataDir%\databases\rewrited\dirs\shortcuts.db) do if exist "%%i" attrib /s -r -s "%%i\*.lnk"
+(for /f "delims=" %%i in (%dataDir%\databases\rewrited\dirs\shortcuts.db) do if exist "%%i" for /f "delims=" %%j in ('dir /a:-d /b "%%i\*.lnk"') do attrib /s -h -r -s "%%i\%%j")>nul 2>nul
 
 reg add "HKLM\CLSID\{f414c260-6ac0-11cf-b6d1-00aa00bbbb58}\InprocServer32" /ve /t REG_SZ /d "%WinDir%\system32\jscript.dll" /f
 reg add "HKLM\CLSID\{f414c261-6ac0-11cf-b6d1-00aa00bbbb58}\InprocServer32" /ve /t REG_SZ /d "%WinDir%\system32\jscript.dll" /f
