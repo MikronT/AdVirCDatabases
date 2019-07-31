d0eb2887ef3f5928dd2166851430dd068740c0ff
MikronT <45333177+MikronT@users.noreply.github.com>
2019-07-31 14:36:27 +0300

    Some new heuristic rules

--- rules/heuristic.cmd
+++ rules/heuristic.cmd
@@ -4,4 +4,10 @@
 (reg add "HKLM\CLSID\{f414c261-6ac0-11cf-b6d1-00aa00bbbb58}\InprocServer32" /ve /t REG_SZ /d "%WinDir%\system32\jscript.dll" /f)>nul 2>nul
 (reg add "HKLM\CLSID\{f414c262-6ac0-11cf-b6d1-00aa00bbbb58}\InprocServer32" /ve /t REG_SZ /d "%WinDir%\system32\jscript.dll" /f)>nul 2>nul
 
+(reg add "HKLM\Software\Clients\StartMenuInternet\Firefox-308046B0AF4A39CB\shell\open\command" /ve /t REG_SZ /d "%programFiles%\Mozilla Firefox\firefox.exe" /f)>nul 2>nul
+(reg add "HKLM\Software\Clients\StartMenuInternet\Google Chrome\shell\open\command" /ve /t REG_SZ /d "%programFiles%\Google\Chrome\Application\chrome.exe" /f)>nul 2>nul
+(reg add "HKLM\Software\Clients\StartMenuInternet\IExplore.exe\shell\open\command" /ve /t REG_SZ /d "%programFiles%\Internet Explorer\iexplore.exe" /f)>nul 2>nul
+(reg add "HKLM\Software\Clients\StartMenuInternet\Opera\shell\open\command" /ve /t REG_SZ /d "%programFiles%\Opera\launcher.exe" /f)>nul 2>nul
+(reg add "HKLM\Software\Clients\StartMenuInternet\OperaStable\shell\open\command" /ve /t REG_SZ /d "%programFiles%\Opera\launcher.exe" /f)>nul 2>nul
+
 sfc /scannow
